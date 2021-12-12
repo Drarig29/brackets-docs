5249df3847cacb167196fe959b2407767f830e32
Corentin Girard <corentingirard.dev@gmail.com>
2021-12-12 23:48:49 +0100

    Finalize pipeline

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+
+# Copy the generated TypeDoc documentations
 mkdir -p ./docs/reference
 cp -r /reference/manager ./docs/reference/
 cp -r /reference/viewer ./docs/reference/
 
+# Run mkdocs with the given args
 mkdocs $@
